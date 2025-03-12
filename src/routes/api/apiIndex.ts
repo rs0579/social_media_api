9947b2325255ee94372597b1e40c601cfb516791
rs0579 <raheemsenegal@outlook.com>
2025-03-12 22:35:15 +0100

    added routes.use to the apiIndex file.

---
+++ src/routes/api/apiIndex.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import {userRoutes} from './userRoutes';
+// import {thoughtRoutes} from './thoughtRoutes';
+
+const router = Router();
+
+router.use('/users', userRoutes);
+// router.use('/thoughts', thoughtRoutes);
