3815514bdcdfb89f15812fa990062dca3ecbcd93
rs0579 <raheemsenegal@outlook.com>
2025-03-13 00:20:41 +0100

    added a thoughtRoutes.ts file, that routes to the various endpoints.

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import { getThoughts, createThought, getThoughtById, updateThoughtById, deleteThoughtById, createReaction, deleteReaction } from '../../controller/thoughtController';
+
+const router = Router();
+
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
