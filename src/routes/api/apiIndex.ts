cdacd7344ff0781cbbd6e2c09d888f5726baeb3d
rs0579 <raheemsenegal@outlook.com>
2025-03-13 00:25:27 +0100

    Finished the routes in the apiIndex.ts file and exported it.

--- src/routes/api/apiIndex.ts
+++ src/routes/api/apiIndex.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import {userRoutes} from './userRoutes';
-import {thoughtRoutes} from './thoughtRoutes';
+import { thoughtRoutes } from './thoughtRoutes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
-/router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+export default router;
