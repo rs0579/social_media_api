1ff8422bc84a137e63080709024b6198b64f65fc
rs0579 <raheemsenegal@outlook.com>
2025-03-12 22:26:18 +0100

    Added a userRoutes.ts

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,17 +1,10 @@
-// import {userSchema} from '../../models/user';
-// import {Router} from 'express';
+import {Router} from 'express';
+import {getUsers, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriendById} from '../../controller/userController';
 
-// const router = Router();
+const router = Router();
 
-// router.route('/')
-//   .get(getAllUsers)
-//   .post(createUser);
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriendById);
 
-// router.route('/:userid')
-//     .get(getUserById)
-//     .put(updateUser)
-//     .delete(deleteUser);
-
-// router.route('/:userid/friends/:friendid')
-//     .post(addFriend)
-//     .delete(deleteFriend);
+export {router as userRoutes}
